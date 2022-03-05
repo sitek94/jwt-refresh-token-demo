b1e28f39836fe70a07e5633b6e75b90250d562cb
Maciek <msitkowski94@gmail.com>
2022-03-05 21:31:03 +0100

    add simple login form, styled with pico.css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,45 @@
 import * as React from 'react'
 
 function App() {
+  const login = (email: string, password: string) => {}
   return (
     <div>
-      <h1>Hello</h1>
-      <p>JWT</p>
+      <hgroup>
+        <h1>Sign in</h1>
+        <h2>And watch the magic happen</h2>
+      </hgroup>
+      <FormLogin onSubmit={login} />
     </div>
+  )
+}
+
+function FormLogin({
+  onSubmit,
+}: {
+  onSubmit(email: string, password: string): void
+}) {
+  const [email, setEmail] = React.useState('maciek@maciek.com')
+  const [password, setPassword] = React.useState('maciek')
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    onSubmit(email, password)
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      />
+      <input
+        type="text"
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+      />
+      <button type="submit">Submit</button>
+    </form>
   )
 }
 
